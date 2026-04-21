926ef32b5d6a170178511bba7ee9dbc953e5be93
3bd-allah <abdullah.nabil5044@gmail.com>
2026-04-21 23:14:37 +0200

    Added context to handle global state

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,15 +1,20 @@
 import { Outlet } from "react-router";
 import Sidebar from "../components/Sidebar";
+import { ProjectsContextProvider, ProjectsContext } from "../store/Projects-context";
+import { use } from "react";
 function App() {
+
   return (
-     <div className="flex h-screen w-full bg-white">
-     <nav className="w-72 flex flex-col mt-8 ">
-      <Sidebar projects />
-     </nav>
-     <main className="flex-1 h-full pt-8 px-12 overflow-y-auto">
-      <Outlet />
-     </main>
-     </div>
+   <div className="flex h-screen w-full bg-white">
+    <ProjectsContextProvider>
+        <nav className="w-72 flex flex-col mt-8 ">
+          <Sidebar />
+        </nav>
+        <main className="flex-1 h-full pt-8 px-12 overflow-y-auto">
+          <Outlet />
+        </main>
+    </ProjectsContextProvider>
+    </div>
   );
 }
 
--- src/store/Projects-context.jsx
+++ src/store/Projects-context.jsx
@@ -1,57 +1,55 @@
 import { createContext, useState } from "react";
 
 export const ProjectsContext = createContext({
-    projects:[],
-    addProject:(project)=>{},
-    removeProject:(id)=>{},
-    renameProject:(id, newTitle)=>{},
-    addTask:(id, newTask)=>{}
-})
+  projects: [],
+  addProject: (project) => {},
+  removeProject: (id) => {},
+  renameProject: (id, newTitle) => {},
+  addTask: (id, newTask) => {},
+});
 
-export function ProjectsContextProvider({children}){
+export function ProjectsContextProvider({ children }) {
+  const [projects, setProjects] = useState([]);
 
-    const [projects, setProjects] = useState()
+  function addProject(project) {
+    setProjects((prevProjects) => [project, ...prevProjects]);
+  }
 
-    function addProject(project){
-        setProjects(prevProjects => setProjects([project, ...prevProjects]))
-    }
+  function removeProject(id) {
+    setProjects((prevProjects) => {
+      return prevProjects.filter((project) => project.id !== id);
+    });
+  }
 
-    function removeProject(id){
-        setProjects(prevProjects =>{
-            return prevProjects.filter(project => project.id !== id)
-        })
-    }
+  function renameProject(id, newTitle) {
+    setProjects((prevProjects) => {
+      return prevProjects.map((project) => {
+        if (project.id === id) {
+          return { ...project, title: newTitle };
+        }
+        return project;
+      });
+    });
+  }
 
-    function renameProject (id, newTitle){
-        setProjects(prevProjects => {
-            return prevProjects.map(project =>{
-                if(project.id === id){
-                    return {...project, title: newTitle};
-                }
-                return project;
-            })
-        })
-    }
+  function addTask(id, newTask) {
+    setProjects((prevProjects) => {
+      return prevProjects.map((project) => {
+        if (project.id === id) {
+          project.tasks.push(newTask);
+          return { ...project };
+        }
+        return project;
+      });
+    });
+  }
 
-    function addTask(id, newTask){
-        setProjects(prevProjects =>{
-            return prevProjects.map(project =>{
-                if(project.id === id ){
-                    project.tasks.push(newTask);
-                    return {...project};
-                }
-                return project
-            })
-        }) 
-    }
-
-    const contextValue={
-        projects: projects, 
-        addProject,
-        removeProject, 
-        renameProject,
-        addTask
-    }
-    return <ProjectsContext value={contextValue}>{children}</ProjectsContext>
-
+  const contextValue = {
+    projects: projects,
+    addProject,
+    removeProject,
+    renameProject,
+    addTask,
+  };
+  return <ProjectsContext value={contextValue}>{children}</ProjectsContext>;
 }
